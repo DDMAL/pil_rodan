195ba5b54acfdff33d61885eeeed686df499afd4
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2021-06-04 09:23:28 -0400

    Update __init__.py
    Changed directory locations to use underscores rather than hyphens.

--- __init__.py
+++ __init__.py
@@ -6,8 +6,8 @@ logger = logging.getLogger('rodan')
 
 from rodan.jobs import module_loader
 
-module_loader('rodan.jobs.pil-rodan.red_filtering')
-module_loader('rodan.jobs.pil-rodan.to_png')
-module_loader('rodan.jobs.pil-rodan.to_tiff')
-module_loader('rodan.jobs.pil-rodan.to_jpeg2000')
-module_loader('rodan.jobs.pil-rodan.resize')
+module_loader('rodan.jobs.pil_rodan.red_filtering')
+module_loader('rodan.jobs.pil_rodan.to_png')
+module_loader('rodan.jobs.pil_rodan.to_tiff')
+module_loader('rodan.jobs.pil_rodan.to_jpeg2000')
+module_loader('rodan.jobs.pil_rodan.resize')
